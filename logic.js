b7a0c94c98f47e2d30916ddceeb5e4721d35b829
Mahnoor <mahnoorsaleem750@gmail.com>
2025-12-28 17:28:33 +0500

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,33 @@
+var username;
+var myName = "Mahnoor Saleem";
+var message = "Hello world!";
+
+alert(message);
+
+// student data-------------
+var stdName = "Mahnoor";
+var stdAge = "19 years old";
+var stdCourse = "Modern Web & App Developnment";
+
+alert(stdName);
+alert(stdAge);
+alert(stdCourse);
+
+// one variable alert-----------
+var pizza = "pizza\n pizz\n piz\n pi\n p";
+
+alert(pizza);
+
+// email and string concatenation--------
+var email = "mahnoorsaleem750@gmail.com";
+
+alert("My email address is" + email);
+
+
+// bookName Concatenation-------
+var book = "A Smarter way to learn JavaScript";
+
+alert("I'm trying to learn from the book" + book);
+
+// browser display--------
+document.write("Yah! I can write HTML content through JavaScript");
